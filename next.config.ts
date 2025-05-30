4ecaa78c309033034aa1be11ba041502f403776d
Vojtěch Škopek <vojtba@gmail.com>
2025-05-30 15:28:05 +0200

    Add remote image patterns to Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [new URL('https://fusiongame.s3.eu-north-1.amazonaws.com/elements/**')],
+  },
 };
 
 export default nextConfig;

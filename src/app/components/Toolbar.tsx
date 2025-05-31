00f657cb757f40065b394447c35cc9efe07bde14
Vojtěch Škopek <39000132+WarriorKnight@users.noreply.github.com>
2025-05-31 12:17:48 +0200

    Refactor toolbar layout and styling for better readability

--- src/app/components/Toolbar.tsx
+++ src/app/components/Toolbar.tsx
@@ -23,21 +23,22 @@ export default function Toolbar({ elements }: ToolbarProps) {
   });
 
   return (
-    <div
-      ref={setNodeRef}
-      className={`rounded-3xl flex flex-wrap m-5 p-2.5 border border-gray-300 gap-2.5 items-center min-h-[76px]
-         transition-colors duration-200
-         ${isOver ? 'border-red-400' : ''}`}
-    >
-      {elements.length > 0 ? (
-        elements.map((el, idx) => (
-          <div key={`${el._id}-${idx}`} className="flex-1 min-w-[50px]">
-            <Element id={`toolbar-${el._id}`} data={el} />
-          </div>
-        ))
-      ) : (
-        !isOver && <p className="text-gray-500">No elements found.</p>
-      )}
+    <div className={`relative m-5 p-1 rounded-3xl border shadow-sm transition-colors duration-200  ${isOver ? 'border-red-400' : 'border-gray-200 '}`}>
+      <div
+        ref={setNodeRef}
+        className={`flex overflow-x-auto py-2 px-2 scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-transparent
+        min-h-[76px] items-center gap-2.5 transition-colors duration-200 rounded-3xl`}
+      >
+        {elements.length > 0 ? (
+          elements.map((el, idx) => (
+            <div key={`${el._id}-${idx}`} className="flex-none w-[70px]">
+              <Element id={`toolbar-${el._id}`} data={el} />
+            </div>
+          ))
+        ) : (
+          !isOver && <p className="text-gray-500 px-2">No elements found.</p>
+        )}
+      </div>
     </div>
   );
 }
